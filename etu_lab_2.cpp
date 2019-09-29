54d2a5a4cb283c0d7d8bf30665b2de3ab9f2d37a
GregoryNevzorov <gregoryofrivia@gmail.com>
2019-09-30 00:52:10 +0300

    void print_array

--- etu_lab_2.cpp
+++ etu_lab_2.cpp
@@ -3,6 +3,30 @@
 #include <chrono>
 using namespace std;
 
+//Функция для вывода массива в консоль
+//Если значение элемента массива меньше 10 (только 1 разряд), перед ним в консоль печатается дополнительный пробел.
+void print_array(int N, int i, int j, int random_array[])
+{
+	for (i; i < N; i++)
+	{
+		j += 1;
+		if (j % 10 == 0)
+		{
+			if (random_array[i] >= 10)
+				cout << random_array[i] << "\n";
+			else
+				cout << " " << random_array[i] << "\n";
+		}
+		else
+		{
+			if (random_array[i] >= 10)
+				cout << random_array[i] << " ";
+			else
+				cout << " " << random_array[i] << " ";
+		}
+	}
+}
+
 int main()
 {
 	char end = 'y';
@@ -12,40 +36,21 @@ int main()
 		const int N = 100;
 		int random_array[N];
 		//Счетчики для циклов for.
-		int j = 0;
 		int i = 0;
-		//Цикл заполняет массив random_array случайными значениями в диапазоне от 0 до 99 (int), затем выводит все элементы массива в консоль, по 10 на строку.
-		//Если значение элемента массива меньше 10 (только 1 разряд), перед ним в консоль печатается дополнительный пробел.
-		cout << "random_array before sorting:\n\n";
+		int j = 0;
+		//Цикл заполняет массив random_array случайными значениями в диапазоне от 0 до 99 (int).
 		srand(time(NULL));
-		for (i; i < N; i++)
-		{
-			random_array[i] = rand() % 100;
-			j += 1;
-			if (j % 10 == 0)
-			{
-				if (random_array[i] >= 10)
-					cout << random_array[i] << "\n";
-				else
-					cout << " " << random_array[i] << "\n";
-			}
-			else
-			{
-				if (random_array[i] >= 10)
-					cout << random_array[i] << " ";
-				else
-					cout << " " << random_array[i] << " ";
-			}
-		}
-		//Сбрасываем переменные i, j.
-		j = 0;
+		for (i; i < N; i++) random_array[i] = rand() % 100;
+		//Сбрасываем переменную i.
 		i = 0;
+		//Теперь вывод массива в консоль вынесен в отдельную функцию - print_array.
+		cout << "random_array before sorting:\n\n";
+		print_array(N, i, j, random_array);
 
 
 		//Bubble sort
 		int counter = N - 1;
 		int exchange;
-		cout << "\nBubble sort:\n\n";
 		do
 		{
 			j = 0;
@@ -66,24 +71,13 @@ int main()
 		counter = 0;
 		exchange = 0;
 		//Распечатываем отсортированный массив
-		for (i; i < N; i++)
-		{
-			j += 1;
-			if (j % 10 == 0)
-			{
-				if (random_array[i] >= 10)
-					cout << random_array[i] << "\n";
-				else
-					cout << " " << random_array[i] << "\n";
-			}
-			else
-			{
-				if (random_array[i] >= 10)
-					cout << random_array[i] << " ";
-				else
-					cout << " " << random_array[i] << " ";
-			}
-		}
+		cout << "\nBubble sort:\n\n";
+		print_array(N, i, j, random_array);
+
+
+		//Shaker sort
+
+
 		//Проверка на повторный запуск программы
 		cout << "Run this program again now? (y / n = any other character)";
 		cin >> end;
